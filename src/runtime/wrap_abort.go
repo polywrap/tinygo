3f6b31d1b5ff4cd4e1da7c3a2fc368500d2cf787
dOrgJelli <jelli@dorg.tech>
2023-07-12 18:13:15 +0200

    fix: properly cast ptr

--- src/runtime/wrap_abort.go
+++ src/runtime/wrap_abort.go
@@ -16,20 +16,20 @@ func __wrap_abort(
 )
 
 func wrapAbort(msg string) {
-	msgPtr := uint32(uintptr(unsafe.Pointer(&msg)))
+	msgPtr := unsafe.Pointer(&msg)
 	msgLen := uint32(len(msg))
 
 	file := "wrap_abort.go"
-	filePtr := uint32(uintptr(unsafe.Pointer(&file)))
+	filePtr := unsafe.Pointer(&file)
 	fileLen := uint32(len(file))
 
 	line := uint32(12)
 	column := uint32(0)
 
 	__wrap_abort(
-		msgPtr,
+		*(*uint32)(msgPtr),
 		msgLen,
-		filePtr,
+		*(*uint32)(filePtr),
 		fileLen,
 		line,
 		column,
